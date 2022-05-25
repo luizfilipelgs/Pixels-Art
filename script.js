625921391c1619cc5bdef91005106918d8fdd6a5
Luiz Filipe <luizfilipelgs@gmail.com>
2022-05-25 19:13:56 -0300

    atualização

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 
-let larguraGrid = 5
-let alturaGrid = 5
 
-for (let index = 0; index < larguraGrid; index+= 1) {
-    let linha = document.createElement("div")
-    document.getElementById("pixel-board").appendChild(linha)
-    linha.classList = "pixel"
-/* for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
-} */
-    
+function criarGrid(larguraGrid,alturaGrid ) {
+   
+    for (let i = 0; i < larguraGrid; i+= 1) {
+        let linha = document.createElement("div")
+        document.getElementById("pixel-board").appendChild(linha)
+        linha.classList = "pixel"
+        for (let i2 = 0; i2 < alturaGrid; i2 += 1) {
+            let coluna = document.createElement("div")
+            document.getElementById("pixel-board").appendChild(coluna)
+            coluna.classList = "pixel"
+    }
+        
+    }
 }
+criarGrid(5,5)
