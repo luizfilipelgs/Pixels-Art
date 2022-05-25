6f55813d57a38b14859800f5e8aedf0872e6036f
Luiz Filipe <luizfilipelgs@gmail.com>
2022-05-25 17:33:47 -0300

    1-3

---
+++ script.js
@@ -0,0 +1,14 @@
+
+let larguraGrid = 5
+let alturaGrid = 5
+
+for (let index = 0; index < larguraGrid; index+= 1) {
+    let linha = document.createElement("div")
+    document.getElementById("pixel-board").appendChild(linha)
+    linha.classList = "pixel"
+/* for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+} */
+    
+}
