87feee02ea2988de7281848d77c4a84cd72d5631
Luiz Filipe <luizfilipelgs@gmail.com>
2022-05-25 23:16:17 -0300

    estilização etapa 1-6

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
-let preto = document.getElementById("preto");
-preto.classList.add( "selected");
-
 window.onload = function () {
+  function criarGrid(larguraGrid, alturaGrid) {
+    let px = larguraGrid * 40 + larguraGrid * 2;
+    let widthStyle = document.querySelector('#pixel-board');
+    widthStyle.style.width = px + 'px';
 
-
-function criarGrid(larguraGrid,alturaGrid ) {
-   
-    for (let i = 0; i < larguraGrid; i+= 1) {
-        let linha = document.createElement("div")
-        document.getElementById("pixel-board").appendChild(linha)
-        linha.classList = "pixel"
-        for (let i2 = 1; i2 < alturaGrid; i2 += 1) {
-            let coluna = document.createElement("div")
-            document.getElementById("pixel-board").appendChild(coluna)
-            coluna.classList = "pixel"
+    for (let i = 0; i < larguraGrid; i += 1) {
+      let linha = document.createElement('div');
+      document.getElementById('pixel-board').appendChild(linha);
+      linha.classList = 'pixel';
+      for (let i2 = 1; i2 < alturaGrid; i2 += 1) {
+        let coluna = document.createElement('div');
+        document.getElementById('pixel-board').appendChild(coluna);
+        coluna.classList = 'pixel';
+      }
     }
-        
-    }
-}
-criarGrid(5,5)
-}
+  }
+  criarGrid(5, 5);
+};
